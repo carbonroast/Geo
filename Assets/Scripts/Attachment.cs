8a845bfa46042256e633ef794410ed0810bc87dc
carbonroast <zergling3@gmail.com>
2019-01-10 22:36:40 -0500

    attachment class added

---
+++ Assets/Scripts/Attachment.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attachment : MonoBehaviour
+{
+    public int health;
+    public int movespeed;
+    public int body_size;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+
+    }
+
+    public virtual void OnDeath()
+    {
+
+    }
+
+}

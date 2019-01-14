456cd686ccfde5901a2a709c26a4babf90794830
carbonroast <zergling3@gmail.com>
2019-01-14 00:51:19 -0500

    added crosshair

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5.0f;
     public GameObject weapon;
     public Vector3 velo;
+    public Texture2D cursor;
     private Rigidbody body;
     private Vector3 inputs;
-
+    private Vector3 cursor_position;
+    private float scale = 5.0f;
+    public bool invert = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        speed = 5.0f;
-
         body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputs.x = Input.GetAxis("Horizontal");
-        inputs.z = Input.GetAxis("Vertical");
-
-        if (Input.GetKeyDown("space"))
+        GetCursor();
+        
+        if (Input.GetButtonDown("Fire1"))
         {
             Ray _ray = MoveCursor();
             Shoot(_ray);
@@ -39,12 +39,26 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void GetCursor()
+    {
+        inputs.x = Input.GetAxis("Horizontal");
+        inputs.z = Input.GetAxis("Vertical");
+        cursor_position.x += inputs.x * scale;
+        cursor_position.z -= inputs.z * scale;
+        Debug.Log("x: " + inputs.x + " z: " + inputs.z);
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(cursor_position.x - 16, cursor_position.z - 16, 200, 200), cursor);
+    }
+
     private void Shoot(Ray ray)
     {
         Quaternion _rotation = Quaternion.LookRotation(ray.direction);
         GameObject _go = Instantiate(weapon);
         BasicLaser _weapon = _go.GetComponent<BasicLaser>();
-        _weapon.position = body.position;
+        _weapon.position = body.position + new Vector3(0,0,0.01f);
         _weapon.rotation = _rotation;
     }
 
@@ -52,9 +66,6 @@ public class PlayerController : MonoBehaviour
     {
         
         Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-
-        Vector3 _spawn_position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
         return _ray;
     }
 }
